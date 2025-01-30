fd131513ec82f40fa3d420427cc4fd400aee7e32
hermesa14 <hermesa14@gmail.com>
2025-01-30 09:06:39 +0100

    Add functionality to save calendar as an image and update styles

--- index.js
+++ index.js
@@ -154,3 +154,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
    createCalendar(parseInt(yearSelect.value));
 });
+document.getElementById('saveAsImage').addEventListener('click', function() {
+    html2canvas(document.getElementById('calendar')).then(function(canvas) {
+        let link = document.createElement('a');
+        link.download = 'calendario.png';
+        link.href = canvas.toDataURL();
+        link.click();
+    });
+});
